da8cb6fd0b94922cdaea716325cc221ed7256e59
“Ray” <b15982003>
2020-12-11 16:58:16 +0800

    RecycleVIew Java

--- app/src/main/java/com/example/recycleviewjava/MainActivity.java
+++ app/src/main/java/com/example/recycleviewjava/MainActivity.java
@@ -25,8 +25,7 @@ public class MainActivity extends AppCompatActivity {
     boolean reflesh = false;
     Timer timer = new Timer();
     Handler handler = new Handler();
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
